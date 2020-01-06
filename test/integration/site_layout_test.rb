a66a49bec276a37550de774dc0f5164eec719b8e
shunta.tokunaga <tokushun109@gmail.com>
2020-01-06 19:28:48 +0900

    Finish layout and routes1

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+
+    get help_path
+    assert_select"title",full_title("Help")
+
+    get about_path
+    assert_select"title",full_title("About")
+
+    get contact_path
+    assert_select"title",full_title("Contact")
+
+    get signup_path
+    assert_select"title",full_title("Sign up")
+  end
+  # test "the truth" do
+  #   assert true
+  # end
+end
